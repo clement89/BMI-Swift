bce5d53bf46b6baa7ac8ec4a093fc310e0a4a8de
clementjoseph89 <clementcj75@gmail.com>
2023-09-05 10:37:43 +0530

    Initial commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Clement on 29/08/2019.
+//  Copyright © 2019 Clement. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
